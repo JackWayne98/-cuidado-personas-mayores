0d72ee9cf6a9d625d229efe8d45bc141b6e27ad1
jjimenez <joseluis.jimenezcevallos@gmail.com>
2025-07-03 00:56:34 -0500

    Cambios

--- backend/src/controllers/notificaciones.controller.js
+++ backend/src/controllers/notificaciones.controller.js
@@ -1,6 +1,9 @@
 const Notificacion = require("../models/notificacion.model.js");
 
-const create = async (req, res) => {};
+const create = async (req, res) => {
+    const {evento_actividad_id, mensaje, fecha_envio, leida} = req.body
+    Notificacion.insert()
+};Cambios
 
 module.exports = { create };
 
