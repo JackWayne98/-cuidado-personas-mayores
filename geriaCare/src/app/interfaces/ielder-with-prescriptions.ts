ea7da51a522f9c0052860209dbbf28f6418c4765
FrancoMarx0420 <131933333+FrancoMarx0420@users.noreply.github.com>
2025-07-04 02:53:56 -0700

    Delete geriaCare directory

--- geriaCare/src/app/interfaces/ielder-with-prescriptions.ts
+++
@@ -1,6 +0,0 @@
-import { Ielder } from "./ielder";
-import { IPrescription } from "./iprescriptions";
-export interface IelderWithPrescriptions {
-  elder: Ielder;
-  prescriptions: IPrescription[];
-}
--- geriaCare/src/app/interfaces/ievent.ts
+++
@@ -1,9 +0,0 @@
-export interface Ievent {
-  actividad_id: number;
-  fecha_inicio: string;
-  fecha_fin: string;
-  recordatorio: boolean;
-  grupo_recurrencia_id?: string;
-  intervalo_horas?: number;
-  repeticiones?: number;
-}
